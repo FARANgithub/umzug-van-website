8e94a0f44aaf5a6cba51ccd05a10727a313be3e1
FARANgithub <164150671+FARANgithub@users.noreply.github.com>
2026-09-09 23:46:27 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // die Assets unter https://<user>.github.io/umzug-van-website/ korrekt findet.
 export default defineConfig({
   plugins: [react()],
-  base: '/umzug-van-website/',
+base: '/',
 })
